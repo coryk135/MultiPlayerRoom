75f2b467d9dfc95f5b260d6e5b95dd08804b141e
sedley <samuel.edley@gmail.com>
2021-10-27 21:27:03 -0700

    Add pause menu, bp character
    This commit adds a functional pause menu and changes the
    third person character to use a blueprint inherited from the C++
    base class.

--- Source/MultiPlayerRoom/MultiPlayerRoomGameMode.cpp
+++ Source/MultiPlayerRoom/MultiPlayerRoomGameMode.cpp
@@ -7,7 +7,7 @@
 AMultiPlayerRoomGameMode::AMultiPlayerRoomGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/MyMultiPlayerRoomCharacterBP"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
